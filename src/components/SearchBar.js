a1e0bf44df8f83d4222009cde2efe49a86655452
Kuddleman <edl1155@gmail.com>
2021-01-09 15:30:33 -0800

    implement onInputChange and onFormSubmit functions in SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,13 +1,29 @@
 import React from 'react' 
 
 class SearchBar extends React.Component {
+
+    state = {term: ''}
+
+    onInputChange = (event) => {
+        this.setState({term: event.target.value})
+    }
+
+    onFormSubmit = event => {
+        event.preventDefault()
+
+        // TODO : make sure we call callback from parent componet
+    }
+
     render() {
         return (
             <div className="search-bar ui segment">
-                <form action="" className="ui form">
+                <form onSubmit={this.onFormSubmit} className="ui form">
                     <div className="field">
                         <label htmlFor="">Video Search</label>
-                        <input type="text"/>
+                        <input type="text" 
+                               value={this.state.term}
+                               onChange={this.onInputChange}
+                        />          
                     </div>
                 </form>
             </div>
