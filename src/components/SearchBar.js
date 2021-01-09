50d29f656ac81e128bb538e8908eb7e7e3d92781
Kuddleman <edl1155@gmail.com>
2021-01-09 15:20:14 -0800

    add SearchBar markup and semantic ui CDN

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -3,7 +3,14 @@ import React from 'react'
 class SearchBar extends React.Component {
     render() {
         return (
-            <div>hello from SEARCH BAR</div>
+            <div className="search-bar ui segment">
+                <form action="" className="ui form">
+                    <div className="field">
+                        <label htmlFor="">Video Search</label>
+                        <input type="text"/>
+                    </div>
+                </form>
+            </div>
         )
     }
 }
