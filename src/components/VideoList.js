47e6e0f2c90e88076a4a12e3fc222a84886a368b
Kuddleman <edl1155@gmail.com>
2021-01-13 17:54:28 -0800

    build videoitem component

--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -1,8 +1,12 @@
 import React from 'react'  
+import VideoItem from './VideoItem'
 
-const VideoList = props => {
+const VideoList = ({videos}) => {
+    const renderedList = videos.map(video =>{
+        return <VideoItem video={video}/>
+    })
     return (
-        <div>I have {props.videos.length} videos</div>
+        <div>{renderedList}</div>
     )
 }
 
